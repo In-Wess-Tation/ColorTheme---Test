291ec20b3f89700decadac46d4da81dbcb383022
Wess L. Toft <weslyn.l.toft@gmail.com>
2025-04-15 10:17:57 +0200

    Booking template, done

---
+++ Pages/ReserveRoom.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ColorTheme___Test.Pages
+{
+    public class ReserveRoomModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Resevations.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ColorTheme___Test.Pages
+{
+    public class ResevationsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
